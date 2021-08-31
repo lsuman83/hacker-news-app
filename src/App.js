03452223caef8ef74418353a3e1f34ea154140dc
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-08-30 23:23:59 -0400

    build Home page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import {
   Route
  } from 'react-router-dom';
 
- import NewsListContainer from '../src/containers/NewsListContainer';
- import Navbar from '../src/components/Navbar'
- import HistoryListContainer from '../src/containers/HistoryListContainer';
-
+import NewsListContainer from '../src/containers/NewsListContainer';
+import Navbar from '../src/components/Navbar'
+import HistoryListContainer from '../src/containers/HistoryListContainer';
+import Home from './Home';
+ 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Home />
         <Switch>
           <Route 
             exact
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div className='text-center italic text-xl'>
+            <h1>
+                Hacker News App
+            </h1>
+        </div>
+    )
+}
